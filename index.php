b9f233e39d174ab6432111f5331b8b0decb9c5f2
gustavo <gustavo.lnkbr@gmail.com>
2024-06-19 18:00:58 -0300

    alteracao index.php

--- index.php
+++ index.php
@@ -17,61 +17,56 @@
     <div id="principal">
         <div class="container">
             <div class="row">
-              <div class="col-12">
-                <table class="table table-bordered">
-                  <thead>
-                    <tr>
-                      <th scope="col">ID</th>
-                      <th scope="col">Nome do Produto</th>
-                      <th scope="col">Descrição</th>
-                      <th scope="col">Quantidade</th>
-                      <th scope="col">Preço</th>
-                      <th scope="col">Categoria</th>
-                    </tr>
-                  </thead>
-                  <tbody>
-                    <tr>
-                      <th scope="row">1</th>
-                      <td>Bootstrap 4 CDN and Starter Template</td>
-                      <td>Cristina</td>
-                      <td>Cristina</td>
-                      <td>Cristina</td>
-                      <td>2.846</td>
-                      <td>
-                        <button type="button" class="btn btn-success">Atualizar<i class="fas fa-edit"></i></button>
-                        <button type="button" class="btn btn-danger"><i class="far fa-trash-alt">Deletar</i></button>
-                      </td>
-                    </tr>
-                    <tr>
-                      <th scope="row">2</th>
-                      <td>Bootstrap Grid 4 Tutorial and Examples</td>
-                      <td>Cristina</td>
-                      <td>3.417</td>
-                      <td>3.417</td>
-                      <td>3.417</td>
-                      <td>
-                        <button type="button" class="btn btn-success">Atualizar<i class="fas fa-edit"></i></button>
-                        <button><a href="delete.php?id="></a></button>
-                      </td>
-                    </tr>
-                    <tr>
-                      <th scope="row">3</th>
-                      <td>Bootstrap Flexbox Tutorial and Examples</td>
-                      <td>Cristina</td>
-                      <td>1.234</td>
-                      <td>1.234</td>
-                      <td>1.234</td>
-                      <td>
-                        <button type="button" class="btn btn-success">Atualizar<i class="fas fa-edit"></i></button>
-                        <button type="button" class="btn btn-danger">Deletar<i class="far fa-trash-alt"></i></button>
-                      </td>
-                    </tr>
-                  </tbody>
-                </table>
-              </div>
+                <div class="col-12">
+                    <table class="table table-bordered">
+                        <thead>
+                            <tr>
+                                <th scope="col">ID</th>
+                                <th scope="col">Nome do Produto</th>
+                                <th scope="col">Descrição</th>
+                                <th scope="col">Quantidade</th>
+                                <th scope="col">Preço</th>
+                                <th scope="col">Categoria</th>
+                                <th scope="col">Ações</th>
+                            </tr>
+                        </thead>
+                        <tbody>
+                            <?php
+                            include 'config.php';
+
+                            // Consulta SQL para buscar os produtos com suas categorias
+                            $sql = "SELECT p.id, p.nome_produto, p.descricao_produto, p.quantidade, p.preco, c.nome_categoria
+                                    FROM produtos p
+                                    INNER JOIN categorias c ON p.categoria_id = c.ID";
+                            $result = $conn->query($sql);
+
+                            if ($result->num_rows > 0) {
+                                // Exibir os produtos encontrados
+                                while ($row = $result->fetch_assoc()) {
+                                    echo '<tr>';
+                                    echo '<th scope="row">' . $row['id'] . '</th>';
+                                    echo '<td>' . $row['nome_produto'] . '</td>';
+                                    echo '<td>' . $row['descricao_produto'] . '</td>';
+                                    echo '<td>' . $row['quantidade'] . '</td>';
+                                    echo '<td>' . $row['preco'] . '</td>';
+                                    echo '<td>' . $row['nome_categoria'] . '</td>';
+                                    echo '<td>
+                                            <button type="button" class="btn btn-success">Atualizar <i class="fas fa-edit"></i></button>
+                                            <button type="button" class="btn btn-danger">Deletar <i class="far fa-trash-alt"></i></button>
+                                          </td>';
+                                    echo '</tr>';
+                                }
+                            } else {
+                                echo '<tr><td colspan="7">Nenhum produto encontrado</td></tr>';
+                            }
+
+                            $conn->close();
+                            ?>
+                        </tbody>
+                    </table>
+                </div>
             </div>
-          </div>
-    </div>
+        </div>
     </div>
 </body>
 </html>
